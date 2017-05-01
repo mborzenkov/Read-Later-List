07255a602ea7a295577b5e1592a3058112df3441
Maxim Borzenkov <mborzenkov-dev@yandex.ru>
2017-05-01 20:18:53 +0400

    EditItem: CheckStyle, Lint, PMD

--- app/src/main/java/com/example/mborzenkov/readlaterlist/activity/EditItemActivity.java
+++ app/src/main/java/com/example/mborzenkov/readlaterlist/activity/EditItemActivity.java
@@ -9,6 +9,7 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -21,9 +22,9 @@ import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import com.example.mborzenkov.readlaterlist.R;
 import com.example.mborzenkov.readlaterlist.adt.ReadLaterItem;
 import com.example.mborzenkov.readlaterlist.adt.ReadLaterItemParcelable;
-import com.example.mborzenkov.readlaterlist.R;
 
 /**
  * Activity для редактирования элемента MainListActivity
@@ -34,12 +35,12 @@ import com.example.mborzenkov.readlaterlist.R;
  */
 public class EditItemActivity extends AppCompatActivity implements View.OnClickListener {
 
-    /** ID для открытия ColorPickerActivity на редактирование цвета */
+    /** ID для открытия ColorPickerActivity на редактирование цвета. */
     private static final int ITEM_EDIT_COLOR_REQUEST = 11;
 
-    /** Текущий выбранный цвет в формате sRGB */
+    /** Текущий выбранный цвет в формате sRGB. */
     private int mChosenColor;
-    /** Признак - создается новый объект или редактируется имеющийся */
+    /** Признак - создается новый объект или редактируется имеющийся. */
     private boolean mNewItem;
 
     // Объекты layout
@@ -59,16 +60,23 @@ public class EditItemActivity extends AppCompatActivity implements View.OnClickL
         setSupportActionBar(toolbar);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_edit_item_save);
         fab.setOnClickListener(this);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         // Инициализация объектов layout
         mLabelEditText = (EditText) findViewById(R.id.et_edit_item_label);
         mLabelEditText.addTextChangedListener(new TextWatcher() {
             // TextChangedListener тут нужен только для сброса ошибок
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) { mLabelInputLayout.setError(null); }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                mLabelInputLayout.setError(null);
+            }
+
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
             @Override
             public void afterTextChanged(Editable s) { }
         });
@@ -79,9 +87,11 @@ public class EditItemActivity extends AppCompatActivity implements View.OnClickL
 
         // Чтение данных из Intent
         Intent intent = getIntent();
-        if (intent != null && intent.hasExtra(ReadLaterItemParcelable.KEY_EXTRA) && intent.hasExtra(ReadLaterItemParcelable.KEY_UID)) {
+        if (intent != null && intent.hasExtra(ReadLaterItemParcelable.KEY_EXTRA)
+                && intent.hasExtra(ReadLaterItemParcelable.KEY_UID)) {
             // В Intent были переданы данные об объекте, записываем их в соответствующие поля
-            ReadLaterItem itemData = ((ReadLaterItemParcelable) intent.getParcelableExtra(ReadLaterItemParcelable.KEY_EXTRA)).getItem();
+            ReadLaterItem itemData =
+                    ((ReadLaterItemParcelable) intent.getParcelableExtra(ReadLaterItemParcelable.KEY_EXTRA)).getItem();
             mLabelEditText.setText(itemData.getLabel());
             mDescriptionEditText.setText(itemData.getDescription());
             mChosenColor = itemData.getColor();
@@ -89,13 +99,14 @@ public class EditItemActivity extends AppCompatActivity implements View.OnClickL
             fab.setImageResource(R.drawable.ic_edit_24dp);
             mNewItem = false;
         } else {
-            // В Intent не переданы данные или что-то пошло не так, в любом случае это новый объект (возможно если что-то идет не так следует вызывать exception)
+            // В Intent не переданы данные или что-то пошло не так, в любом случае это новый объект
+            // (возможно если что-то идет не так следует вызывать exception)
             mNewItem = true;
             getSupportActionBar().setTitle(getString(R.string.edititem_title_add));
             fab.setImageResource(R.drawable.ic_add_24dp);
             mChosenColor = ContextCompat.getColor(this, R.color.item_default_color);
             // Устанавливаем фокус и открываем клавиатуру на редактирование Label, чтобы все было красиво и удобно
-            if(mLabelEditText.requestFocus()) {
+            if (mLabelEditText.requestFocus()) {
                 getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
             }
         }
@@ -162,7 +173,8 @@ public class EditItemActivity extends AppCompatActivity implements View.OnClickL
         }
     }
 
-    public void finishEditing() {
+    /** Выполняет сохранение. */
+    private void finishEditing() {
         // packInputData возвращает null, если что-то не так
         ReadLaterItem resultData = packInputData();
         if (resultData != null) {
@@ -173,15 +185,14 @@ public class EditItemActivity extends AppCompatActivity implements View.OnClickL
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // Принимает данные из ColorPickerActivity, сохраняет их
-        if (resultCode == RESULT_OK && requestCode == ITEM_EDIT_COLOR_REQUEST && data != null && data.hasExtra(ColorPickerActivity.CHOSEN_KEY)) {
+        if (resultCode == RESULT_OK && requestCode == ITEM_EDIT_COLOR_REQUEST
+                && data != null && data.hasExtra(ColorPickerActivity.CHOSEN_KEY)) {
             mChosenColor = data.getIntExtra(ColorPickerActivity.CHOSEN_KEY, Color.TRANSPARENT);
             updateChosenColor();
         }
     }
 
-    /**
-     * Устанавливает цвет ImageButton на mChosenColor
-     */
+    /** Устанавливает цвет ImageButton на mChosenColor. */
     private void updateChosenColor() {
         ((GradientDrawable) mColorImageButton.getBackground()).setColor(mChosenColor);
     }
@@ -199,18 +210,20 @@ public class EditItemActivity extends AppCompatActivity implements View.OnClickL
         // TODO: В ColorPickerActivity убрать mFromIntent из полей, потому что он не нужен
         // TODO: Прогнать весь проект через lint, checkstyle и другие
         // TODO: Написать тесты Robolectric или Mockito
-        // TODO: Разбить приложение по MVP / M getUsername, V findViewById, P вызовы // Отделить логику от взаимодействия с ОС и тестировать по отдельности
+        // TODO: Разбить приложение по MVP / M getUsername, V findViewById, P вызовы
+        // TODO: Отделить логику от взаимодействия с ОС и тестировать по отдельности
         Intent resultIntent = new Intent();
         resultIntent.putExtra(ReadLaterItemParcelable.KEY_EXTRA, new ReadLaterItemParcelable(resultData));
         if (getIntent().hasExtra(ReadLaterItemParcelable.KEY_UID)) {
-            resultIntent.putExtra(ReadLaterItemParcelable.KEY_UID, getIntent().getIntExtra(ReadLaterItemParcelable.KEY_UID, -1));
+            resultIntent.putExtra(ReadLaterItemParcelable.KEY_UID,
+                    getIntent().getIntExtra(ReadLaterItemParcelable.KEY_UID, -1));
         }
         setResult(RESULT_OK, resultIntent);
         finish();
     }
 
-    /**
-     * Превращает данные формы в объект ReadLaterItem
+    /** Превращает данные формы в объект ReadLaterItem.
+     *
      * @return Объект ReadLaterItem или null, если не удалось превратить данные в объект
      *      Например, если не заполнен Label
      */
