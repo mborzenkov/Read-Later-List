63c07f5762591389a31f2aab3ae19801951053fc
Maxim Borzenkov <mborzenkov-dev@yandex.ru>
2017-05-16 15:33:28 +0500

    Added Cloud API
    Cloud API in development, no usages yet
    But tests are passing

---
+++ app/src/main/java/com/example/mborzenkov/readlaterlist/networking/CloudSyncUtils.java
@@ -0,0 +1,87 @@
+package com.example.mborzenkov.readlaterlist.networking;
+
+import android.graphics.Color;
+import android.util.Log;
+
+import com.example.mborzenkov.readlaterlist.adt.ReadLaterItem;
+import com.example.mborzenkov.readlaterlist.adt.ReadLaterItemJsonAdapter;
+import com.example.mborzenkov.readlaterlist.adt.UserInfo;
+import com.squareup.moshi.Moshi;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
+public class CloudSyncUtils {
+
+    // TODO: Отправка данных на сервер асинхронно
+    // TODO: Сохранение даты старте синхронизации в переменную, получение LastSync из SharedPreferences
+    // TODO: Выполнение синхронизации
+
+    // Получили список всех заметок с сервера, сохранили
+    //
+    // Цикл по всем заметкам сервера
+    //      LastModifiedServer > LastSync
+    //          Есть в локальной базе
+    //              LastModifiedLocal <= LastSync
+    //                  Update Local
+    //              Иначе
+    //                  - Конфликт, меняем modified на lastSync + 1
+    //          Иначе
+    //              Insert Local
+    //      Иначе
+    //          Есть в локальной базе
+    //              continue; (случай разбирается дальше)
+    //          Иначе
+    //              Delete Server
+    //
+    // Цикл по всем заметкам локальным, кроме уже проверенных
+    //      LastModifiedLocal > LastSync
+    //          Есть на сервере
+    //              LastModifiedServer <= LastSync
+    //                  Update Server
+    //              Иначе
+    //                  - Конфликт, меняем modified на lastSync + 1
+    //          Иначе
+    //              Insert Server
+    //              Update Local itemId
+    //      Иначе
+    //          Есть на сервере
+    //              continue; (изменений нет ни на сервере, ни локально)
+    //          Иначе
+    //              Delete Local
+    //
+    // Если выше все успешно, записываем StartSync в SharedPreferences
+    //
+    // Обработка конфликтов - показать список заметок (если не принять изменения, при следующей синхронизации
+    // снова вылезут эти же конфликты)
+
+    // TODO: При успешном завершении - обновление SharedPreferences
+
+    public static boolean startFullSync() {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        Moshi moshi = new Moshi.Builder().add(new ReadLaterItemJsonAdapter()).build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(CloudApiYufimtsev.BASE_URL)
+                .client(client)
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .build();
+
+        CloudApiYufimtsev cloudApi = retrofit.create(CloudApiYufimtsev.class);
+
+        return false;
+    }
+
+}
---
+++ app/src/test/java/com/example/mborzenkov/readlaterlist/CloudApiTest.java
@@ -0,0 +1,138 @@
+package com.example.mborzenkov.readlaterlist;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import android.graphics.Color;
+
+import com.example.mborzenkov.readlaterlist.adt.ReadLaterItem;
+import com.example.mborzenkov.readlaterlist.adt.ReadLaterItemJsonAdapter;
+import com.example.mborzenkov.readlaterlist.networking.CloudApiYufimtsev;
+import com.example.mborzenkov.readlaterlist.networking.CloudApiYufimtsev.DefaultResponse;
+import com.example.mborzenkov.readlaterlist.networking.CloudApiYufimtsev.AllItemsResponse;
+import com.example.mborzenkov.readlaterlist.networking.CloudApiYufimtsev.NewItemResponse;
+import com.example.mborzenkov.readlaterlist.networking.CloudApiYufimtsev.SingleItemResponse;
+import com.squareup.moshi.Moshi;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+import org.junit.Test;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
+
+/** Тестирует подключение к Cloud API. */
+@SuppressWarnings("FieldCanBeLocal") // Поля вынесены на уровень класса для улучшенной читаемости"
+public class CloudApiTest {
+
+    private static final int TEST_USER_ID = 100593;
+    private static final int RESPONSE_CODE_SUCCESS = 200;
+
+    private static final long currentTime = System.currentTimeMillis();
+    private static final String normalImageUrl = "http://i.imgur.com/aYioFT9.jpg";
+    private static final ReadLaterItem item1 = new ReadLaterItem("Заголовок 1", "Descrip 232", Color.RED,
+                                                           currentTime, currentTime, currentTime, normalImageUrl);
+    private static final ReadLaterItem item2 = new ReadLaterItem("Заголовок 2", "Desc 22", Color.BLUE,
+                                                            currentTime, currentTime + 10, currentTime, normalImageUrl);
+
+    @Test
+    public void testApiYufimtsev() throws IOException {
+
+        Moshi moshi = new Moshi.Builder().add(new ReadLaterItemJsonAdapter()).build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(CloudApiYufimtsev.BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .build();
+
+        CloudApiYufimtsev cloudApi = retrofit.create(CloudApiYufimtsev.class);
+
+        {
+            Response<AllItemsResponse> responseAllItems =
+                    cloudApi.getAllItems(TEST_USER_ID).execute();
+            assertEquals(responseAllItems.code(), RESPONSE_CODE_SUCCESS);
+            AllItemsResponse responseAllItemsData = responseAllItems.body();
+            assertTrue(responseAllItemsData != null);
+            assertEquals(responseAllItemsData.status, CloudApiYufimtsev.STATUS_SUCCESS);
+            assertEquals(responseAllItemsData.error, null);
+            assertTrue(responseAllItemsData.data != null);
+        }
+
+        int newItemId;
+        {
+            Response<CloudApiYufimtsev.NewItemResponse> newItemResponse =
+                    cloudApi.createItem(TEST_USER_ID, item1).execute();
+            assertEquals(newItemResponse.code(), RESPONSE_CODE_SUCCESS);
+            NewItemResponse responseAllItemsData = newItemResponse.body();
+            assertTrue(responseAllItemsData != null);
+            assertEquals(responseAllItemsData.status, CloudApiYufimtsev.STATUS_SUCCESS);
+            assertEquals(responseAllItemsData.error, null);
+            assertTrue(responseAllItemsData.data != null);
+            assertTrue(responseAllItemsData.data > 0);
+            newItemId = responseAllItemsData.data;
+        }
+
+        {
+            Response<CloudApiYufimtsev.SingleItemResponse> singleItemResponse =
+                    cloudApi.getItem(TEST_USER_ID, newItemId).execute();
+            assertEquals(singleItemResponse.code(), RESPONSE_CODE_SUCCESS);
+            SingleItemResponse responseAllItemsData = singleItemResponse.body();
+            assertTrue(responseAllItemsData != null);
+            assertEquals(responseAllItemsData.status, CloudApiYufimtsev.STATUS_SUCCESS);
+            assertEquals(responseAllItemsData.error, null);
+            assertTrue(responseAllItemsData.data != null);
+            assertTrue(responseAllItemsData.data.equals(item1));
+        }
+
+        {
+            Response<CloudApiYufimtsev.DefaultResponse> updateItemResponse =
+                    cloudApi.updateItem(TEST_USER_ID, newItemId, item2).execute();
+            assertEquals(updateItemResponse.code(), RESPONSE_CODE_SUCCESS);
+            DefaultResponse responseAllItemsData = updateItemResponse.body();
+            assertTrue(responseAllItemsData != null);
+            assertEquals(responseAllItemsData.status, CloudApiYufimtsev.STATUS_SUCCESS);
+            assertEquals(responseAllItemsData.error, null);
+        }
+
+        {
+            Response<CloudApiYufimtsev.SingleItemResponse> singleItemResponse =
+                    cloudApi.getItem(TEST_USER_ID, newItemId).execute();
+            assertEquals(singleItemResponse.code(), RESPONSE_CODE_SUCCESS);
+            SingleItemResponse responseAllItemsData = singleItemResponse.body();
+            assertTrue(responseAllItemsData != null);
+            assertEquals(responseAllItemsData.status, CloudApiYufimtsev.STATUS_SUCCESS);
+            assertEquals(responseAllItemsData.error, null);
+            assertTrue(responseAllItemsData.data != null);
+            assertTrue(responseAllItemsData.data.equals(item2));
+        }
+
+        {
+            Response<CloudApiYufimtsev.DefaultResponse> singleItemResponse =
+                    cloudApi.deleteItem(TEST_USER_ID, newItemId).execute();
+            assertEquals(singleItemResponse.code(), RESPONSE_CODE_SUCCESS);
+            DefaultResponse responseAllItemsData = singleItemResponse.body();
+            assertTrue(responseAllItemsData != null);
+            assertEquals(responseAllItemsData.status, CloudApiYufimtsev.STATUS_SUCCESS);
+            assertEquals(responseAllItemsData.error, null);
+        }
+
+        {
+            Response<CloudApiYufimtsev.SingleItemResponse> singleItemResponse =
+                    cloudApi.getItem(TEST_USER_ID, newItemId).execute();
+            assertEquals(singleItemResponse.code(), RESPONSE_CODE_SUCCESS);
+            SingleItemResponse responseAllItemsData = singleItemResponse.body();
+            assertTrue(responseAllItemsData != null);
+            assertEquals(responseAllItemsData.status, CloudApiYufimtsev.STATUS_ERROR);
+            assertTrue(responseAllItemsData.error != null);
+            assertTrue(responseAllItemsData.data == null);
+        }
+
+    }
+
+}
